34bb8c848f9852fc63c59459c776c969463d3c5d
Tommy <tommy@iamnotagoodartist.com>
2025-04-03 22:22:07 -0700

    time for bed this place is a mess!

--- arduino/bananajam/bananajam.ino
+++ arduino/bananajam/bananajam.ino
@@ -14,29 +14,70 @@ ArduboyTones sound(arduboy.audio.enabled);
 // * count score
 // * fix buggy initial momentum from 0
 
-const int frameRate = 24;
+const int frameRate = 1;
 
 const int rockingAmplitudeDegrees = 90;
 const int rockingFrequencyMs = 1500;
-const float momentumDropPerStep = .0125;
+const float momentumDrop = .025;
 
-const int stepsPerRock = frameRate * (rockingFrequencyMs / 1000.0);
-const int leftwardStepIndex = stepsPerRock / 2;
-const int rightwardStepIndex = 0;
+// TODO: what's the technical term for a side of the swing?
+// TODO: try stepsRemainingInRock
+const int stepsPerRock = 8; // (frameRate * (rockingFrequencyMs / 1000.0)) / 2;
+
+enum Direction { IDLE, LEFT, RIGHT };
 
 int radius = 25;
 int coverage = 170;
 int rotation = 0;
 int step = 0;
 float momentum = 0;
-int direction = 0; // -1 = left, 1 = right
+Direction direction = Direction::IDLE;
 
-bool showStats = false;
+bool showStats = true;
 bool hasPlayedStopSound = true;
 
 // https://github.com/nicolausYes/easing-functions
+double easeInOutSine(double t) {
+  return 0.5 * (1 + sin(3.1415926 * (t - 0.5)));
+}
 double easeInOutQuad(double t) {
   return t < 0.5 ? 2 * t * t : t * (4 - 2 * t) - 1;
+}
+double easeInOutCubic(double t) {
+  return t < 0.5 ? 4 * t * t * t : 1 + (--t) * (2 * (--t)) * (2 * t);
+}
+double easeInOutQuart(double t) {
+  if (t < 0.5) {
+    t *= t;
+    return 8 * t * t;
+  } else {
+    t = (--t) * t;
+    return 1 - 8 * t * t;
+  }
+}
+double easeInOutQuint(double t) {
+  double t2;
+  if (t < 0.5) {
+    t2 = t * t;
+    return 16 * t * t2 * t2;
+  } else {
+    t2 = (--t) * t;
+    return 1 + 16 * t * t2 * t2;
+  }
+}
+double easeInOutExpo(double t) {
+  if (t < 0.5) {
+    return (pow(2, 16 * t) - 1) / 510;
+  } else {
+    return 1 - 0.5 * pow(2, -16 * (t - 0.5));
+  }
+}
+double easeInOutCirc(double t) {
+  if (t < 0.5) {
+    return (1 - sqrt(1 - 2 * t)) * 0.5;
+  } else {
+    return (1 + sqrt(2 * t - 1)) * 0.5;
+  }
 }
 
 void setup() {
@@ -88,43 +129,33 @@ void drawBanana(int bottomCoverage, int bottomRadius, int rotation, int x,
 // * extract consts, direction setting
 // * rate limit
 // * DRY momentum drop
-void handleInputs(float initialMomentum = .25, float momentumIncrement = 1.1) {
+void handleInputs(float initialMomentum = .5, float momentumIncrement = 1.1) {
   arduboy.pollButtons();
 
   if (arduboy.pressed(LEFT_BUTTON)) {
-    // if (!sound.playing()) {
-    //   sound.tones(MOVE_TONES);
-    // }
-
     if (momentum == 0.0) {
       momentum = initialMomentum;
-      direction = -1;
-      step = leftwardStepIndex - stepsPerRock / 4;
-    } else if (direction == 1) {
-      momentum *= (momentumIncrement - 1);
-    } else {
-      momentum *= momentumIncrement;
+      direction = Direction::LEFT;
+      // } else {
+      //   momentum *= momentumIncrement;
     }
   } else if (arduboy.pressed(RIGHT_BUTTON)) {
-    // if (!sound.playing()) {
-    //   sound.tones(MOVE_TONES);
-    // }
-
-    // TODO: fix initial rightward movement not matching leftward
     if (momentum == 0.0) {
       momentum = initialMomentum;
-      direction = 1;
-      step = rightwardStepIndex - stepsPerRock / 4;
-    } else if (direction == -1) {
-      momentum *= (momentumIncrement - 1);
-    } else {
-      momentum *= momentumIncrement;
+      direction = Direction::RIGHT;
+      // } else {
+      //   momentum *= momentumIncrement;
     }
   }
 
   if (arduboy.pressed(DOWN_BUTTON)) {
     sound.tones(CROUCH_TONES);
     momentum /= 2;
+
+    if (momentum == 0) {
+      direction == Direction::IDLE;
+      step = 0;
+    }
   }
 
   if (arduboy.justPressed(A_BUTTON)) {
@@ -144,26 +175,60 @@ void loop() {
   float linearDeviation = 0;
   float easedDeviation = 0;
 
-  if (momentum > 0 || rotation != 0) {
-    step = (step + 1) % stepsPerRock;
-    momentum = max(0, momentum - momentumDropPerStep);
+  // fix it getting stuck on last step twice
+  if (direction != Direction::IDLE && step < (stepsPerRock - 1) &&
+      momentum > 0) {
+    linearDeviation = float(step) / (stepsPerRock - 1);
+    // linearDeviation = abs((float(step) / (stepsPerRock - 1)) - 1.0 / 2) * 2;
 
-    linearDeviation = abs((float(step) / stepsPerRock) - 1.0 / 2) * 2;
-    easedDeviation = easeInOutQuad(linearDeviation);
+    // easedDeviation = easeInOutSine(linearDeviation); // naw
+    // easedDeviation = easeInOutQuad(linearDeviation); // eh
+    // easedDeviation = easeInOutCubic(linearDeviation); // disappears?
 
-    direction = step < leftwardStepIndex ? -1 : 1;
+    // TODO: get this looking at least linearly correct
+    // A big part of the problem is that the start and rest are the same,
+    // which looks like a pause when 2+ cycles are back to back
+    easedDeviation = linearDeviation;
 
-    rotation = momentum * (easedDeviation * rockingAmplitudeDegrees * 2 -
-                           rockingAmplitudeDegrees);
+    // None of these are good
+    // easedDeviation = easeInOutQuart(linearDeviation);
+    // easedDeviation = easeInOutQuint(linearDeviation);
+    // easedDeviation = easeInOutExpo(linearDeviation);
+    // easedDeviation = easeInOutCirc(linearDeviation);
+
+    // Ugh, now how'd I break this one?
+    rotation = momentum * easedDeviation * rockingAmplitudeDegrees;
+
+    if (direction == Direction::LEFT) {
+      rotation = rotation - 90;
+      // rotation = -rotation;
+    } else if (direction == Direction::RIGHT) {
+      rotation = 90 - rotation;
+      // rotation = rotation;
+    }
+
+    step += 1;
   } else {
-    direction = 0;
+    step = 0;
+    momentum = max(0, momentum - .1);
+    rotation = 0;
+
+    if (momentum > 0) {
+      if (direction == Direction::LEFT) {
+        direction = Direction::RIGHT;
+      } else if (direction == Direction::RIGHT) {
+        direction = Direction::LEFT;
+      }
+    } else {
+      direction = Direction::IDLE;
+    }
   }
 
-  if ((linearDeviation == .5 && !sound.playing()) ||
-      momentum == 0 && !hasPlayedStopSound) {
-    sound.tones(CROUCH_TONES);
-    hasPlayedStopSound = true;
-  }
+  // if ((linearDeviation == .5 && !sound.playing()) ||
+  //     momentum == 0 && !hasPlayedStopSound) {
+  //   sound.tones(CROUCH_TONES);
+  //   hasPlayedStopSound = true;
+  // }
 
   handleInputs();
 
@@ -171,13 +236,13 @@ void loop() {
 
   if (showStats) {
     arduboy.setCursor(0, 0);
-    arduboy.print(linearDeviation);
+    arduboy.print(float(momentum));
     arduboy.setCursor(30, 0);
-    arduboy.print(sound.playing());
+    arduboy.print(easedDeviation);
     arduboy.setCursor(60, 0);
-    arduboy.print(direction);
+    arduboy.print(rockingAmplitudeDegrees);
     arduboy.setCursor(90, 0);
-    arduboy.print(momentum);
+    arduboy.print(rotation);
   }
 
   drawBanana(coverage, radius, rotation, center.x + getX(), center.y);
