2e42cf73f04de4eca462ad56c96bdcadb6a73642
Tommy <tommy@iamnotagoodartist.com>
2025-04-04 23:16:53 -0700

    better but worse too

--- arduino/bananajam/bananajam.ino
+++ arduino/bananajam/bananajam.ino
@@ -9,19 +9,23 @@ ArduboyTones sound(arduboy.audio.enabled);
 
 // TODO:
 // * optimize variable types
+// * see about inlining math functions
 // * deal w/ 1+ momentum
 // * sound effects
-// * count score
-// * fix buggy initial momentum from 0
+// * count score -> detented progress bar. animate progress?
+// * Display struct
+// * Banana struct w/ display, momentum, amplitude, frequency; ints > float math
+// * -> banana maker to find values, test difficulty
+// * try filling body white by keeping track of top/bottomYs
 
 const int frameRate = 30;
 
 const int rockingAmplitudeDegrees = 90;
 const int rockingFrequencyMs = 1000;
-const float momentumDrop = .33;
-const float minMomentum = .05;
-float initialMomentum = .25;
-float momentumIncrement = 1.1;
+const float momentumDropPerFrame = .05;
+const float minMomentum = .01;
+float initialMomentum = .15;
+float momentumIncrement = 1.15;
 
 const int stepsPerRock = (frameRate * (rockingFrequencyMs / 1000.0)) / 2;
 
@@ -32,7 +36,9 @@ int coverage = 170;
 int rotation = 0;
 int step = 0;
 float momentum = 0;
+float weight = 0;
 Side side = Side::CENTER;
+Side direction = Side::CENTER;
 
 bool showStats = true;
 
@@ -41,15 +47,28 @@ void setup() {
   arduboy.waitNoButtons();
   arduboy.setFrameRate(frameRate);
 
-  for (int i = 0; i < 10; ++i) {
-    Serial.print("i:");
-    Serial.print(i);
-    Serial.print("\tlinear:");
-    Serial.print(getLinearDeviation(i, 10));
-    Serial.print("\teased:");
-    Serial.print(getEasedDeviation(i, 10));
-    Serial.println();
-  }
+  // for (int i = 0; i < stepsPerRock; ++i) {
+  //   Serial.print("i:");
+  //   Serial.print(i);
+  //   Serial.print("\tlinear:");
+  //   Serial.print(getLinearDeviation(Side::CENTER, i, stepsPerRock));
+  //   Serial.print("\teased:");
+  //   Serial.print(getEasedDeviation(Side::CENTER, i, stepsPerRock));
+  //   Serial.println();
+  // }
+
+  Serial.print("getWeight(LEFT, LEFT)\t");
+  Serial.print(getWeight(Side::LEFT, Side::LEFT, 10, 10));
+  Serial.println();
+  Serial.print("getWeight(LEFT, RIGHT)\t");
+  Serial.print(getWeight(Side::LEFT, Side::RIGHT, 10, 10));
+  Serial.println();
+  Serial.print("getWeight(RIGHT, LEFT)\t");
+  Serial.print(getWeight(Side::RIGHT, Side::LEFT, 10, 10));
+  Serial.println();
+  Serial.print("getWeight(RIGHT, RIGHT)\t");
+  Serial.print(getWeight(Side::RIGHT, Side::RIGHT, 10, 10));
+  Serial.println();
 }
 
 struct Xy {
@@ -70,6 +89,7 @@ void drawSemiCircle(int startingAngle, int coverage, int radius, int x, int y) {
   }
 }
 
+// TODO: a little stem should be easy enough
 void drawBanana(int bottomCoverage, int bottomRadius, int rotation, int x,
                 int y) {
   // NOTE: unapologetically magic values here, who cares
@@ -98,50 +118,100 @@ void drawBanana(int bottomCoverage, int bottomRadius, int rotation, int x,
 void handleInputs() {
   arduboy.pollButtons();
 
+  // weight response looks like .5 to 1
+  // which we want to map to .9 to 1
+
+  // these numbers feel good but don't make sense
+  // like, both sides peak at 1.1 when holding fixed direction. is that right?
+  // whatever the case, holding LEFT shouldn't work
+
   if (arduboy.pressed(LEFT_BUTTON)) {
     if (momentum <= minMomentum) {
       momentum = initialMomentum;
       side = Side::LEFT;
     } else {
-      momentum *= momentumIncrement;
+      weight =
+          ((getWeight(Side::LEFT, direction, step, stepsPerRock) - .5) * 2.0) *
+              .15 +
+          .85;
+      momentum *= momentumIncrement * weight;
     }
   } else if (arduboy.pressed(RIGHT_BUTTON)) {
     if (momentum <= minMomentum) {
       momentum = initialMomentum;
       side = Side::RIGHT;
     } else {
-      momentum *= momentumIncrement;
+      weight =
+          ((getWeight(Side::RIGHT, direction, step, stepsPerRock) - .5) * 2.0) *
+              .15 +
+          .85;
+      momentum *= momentumIncrement * weight;
     }
   }
 
   if (arduboy.pressed(DOWN_BUTTON)) {
     sound.tones(CROUCH_TONES);
-    slowDown();
+    slowDown(.5);
   }
 
   if (arduboy.justPressed(A_BUTTON)) {
     showStats = !showStats;
   }
 
-  momentum = min(1.25, max(-.25, momentum));
+  momentum = min(1, max(0, momentum));
 }
 
 inline int getX() { return (rotation / 360.0) * radius * 2 * M_PI; }
 
-const float getLinearDeviation(int i, int count) {
+// TODO: with a fresh head, really figure out deviation math here
+float getWeight(Side targetDirection, Side currentDirection, int i, int count) {
+  if (currentDirection == RIGHT) {
+    if (targetDirection == Side::RIGHT) {
+      // return 1;
+      return getLinearDeviation(Side::LEFT, Side::RIGHT, i, count);
+    } else if (targetDirection == Side::LEFT) {
+      // return .9;
+      return (1 - getLinearDeviation(Side::RIGHT, Side::RIGHT, i, count) * 1);
+    }
+  } else if (currentDirection == LEFT) {
+    if (targetDirection == Side::RIGHT) {
+      // return .9;
+      return (1 - getLinearDeviation(Side::RIGHT, Side::RIGHT, i, count) * 1);
+    } else if (targetDirection == Side::LEFT) {
+      // return 1;
+      return getLinearDeviation(Side::LEFT, Side::RIGHT, i, count);
+    }
+  }
+
+  return .5;
+}
+
+const float getLinearDeviation(Side fromSide, Side toSide, int i, int count) {
+  if (fromSide == Side::LEFT) {
+    if (toSide == Side::LEFT) {
+      return (1 - getLinearDeviation(Side::CENTER, toSide, i, count)) / 2;
+    } else if (toSide == Side::RIGHT) {
+      return .5 + getLinearDeviation(Side::CENTER, toSide, i, count) / 2;
+    }
+
+    return .5;
+  } else if (fromSide == Side::RIGHT) {
+    return 1 - getLinearDeviation(Side::LEFT, toSide, i, count);
+  }
+
   return 1 - abs((float(i) / count) - .5) * 2;
 }
 
-const float getEasedDeviation(float i, int count) {
+const float getEasedDeviation(Side fromSide, Side toSide, float i, int count) {
   // easeInSine from https://github.com/nicolausYes/easing-functions
-  return sin(1.5707963 * (1 - abs((float(i) / count) - .5) * 2));
+  return sin(1.5707963 * getLinearDeviation(fromSide, toSide, i, count));
 }
 
-// TODO: implement
+// TODO: implement, require some new minimum momentum
 void scorePoint() { sound.tones(CROUCH_TONES); }
 
-void slowDown() {
-  momentum = momentum * (1.0 - momentumDrop);
+void slowDown(float drop) {
+  momentum = momentum * (1.0 - drop);
 
   if (momentum <= minMomentum) {
     momentum = 0;
@@ -149,6 +219,7 @@ void slowDown() {
   }
 }
 
+// TODO: extract stuff
 void loop() {
   if (!(arduboy.nextFrame())) {
     return;
@@ -157,8 +228,8 @@ void loop() {
   float linearDeviation = 0;
   float easedDeviation = 0;
 
-  linearDeviation = getLinearDeviation(step, stepsPerRock);
-  easedDeviation = getEasedDeviation(step, stepsPerRock);
+  linearDeviation = getLinearDeviation(Side::CENTER, side, step, stepsPerRock);
+  easedDeviation = getEasedDeviation(Side::CENTER, side, step, stepsPerRock);
 
   rotation = momentum * easedDeviation * rockingAmplitudeDegrees;
   if (side == Side::LEFT) {
@@ -167,12 +238,18 @@ void loop() {
 
   if (momentum > 0) {
     step += 1;
+    slowDown(momentumDropPerFrame);
 
-    // TODO: try slowDown more frequently
+    if (step == stepsPerRock / 2) {
+      if (side == Side::LEFT) {
+        direction = Side::RIGHT;
+      } else if (side == Side::RIGHT) {
+        direction = Side::LEFT;
+      }
+    }
 
     if (step == stepsPerRock) {
       step = 0;
-      slowDown();
 
       if (momentum > minMomentum) {
         scorePoint();
@@ -186,6 +263,7 @@ void loop() {
     }
   } else {
     side = Side::CENTER;
+    direction = Side::CENTER;
     rotation = 0;
   }
 
@@ -195,15 +273,13 @@ void loop() {
 
   if (showStats) {
     arduboy.setCursor(0, 0);
-    arduboy.print(step);
-    arduboy.setCursor(20, 0);
-    arduboy.print(side);
-    arduboy.setCursor(30, 0);
     arduboy.print(float(momentum));
-    arduboy.setCursor(60, 0);
-    arduboy.print(easedDeviation);
-    arduboy.setCursor(90, 0);
-    arduboy.print(rockingAmplitudeDegrees);
+    arduboy.setCursor(35, 0);
+    arduboy.print(getLinearDeviation(Side::LEFT, side, step, stepsPerRock));
+    arduboy.setCursor(70, 0);
+    arduboy.print(getLinearDeviation(Side::RIGHT, side, step, stepsPerRock));
+    arduboy.setCursor(105, 0);
+    arduboy.print(weight);
   }
 
   drawBanana(coverage, radius, rotation, center.x + getX(), center.y);
